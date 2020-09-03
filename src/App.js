73b5502c3f2ecb90606e5cd22c816db15e1bfa9a
Yussif Issah <djiceman.gh@gmail.com>
2020-09-03 00:34:14 +0000

    MenuComponent2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante ConFusion</NavbarBrand>
