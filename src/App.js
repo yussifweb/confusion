c543c47953631e855eb3b3a23a471209780f3801
Yussif <djiceman.gh@gmail.com>
2020-09-03 14:01:33 +0000

    Menucomponents Pt 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import MenuComponent from './components/MenuComponent';
-import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante ConFusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <MenuComponent />
-    </div>
-  );
+import './App.css';
+import { DISHES } from './shared/dishes';
+import DishDetail from './components/DishDetail';
+
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       dishes: DISHES
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante ConFusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <MenuComponent dishes={this.state.dishes} />
+        {/* <DishDetail></DishDetail> */}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
